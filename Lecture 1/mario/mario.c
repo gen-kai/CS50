cfe1519c59a2143c699d1e3eb18b515f46b11348
gen-kai <87318319+gen-kai@users.noreply.github.com>
2025-06-03 14:39:22 -0700

    Cleanup of the mario solution

--- Lecture 1/mario/mario.c
+++ Lecture 1/mario/mario.c
@@ -1,59 +1,77 @@
 #include <stdio.h>
 
-static void PrintChar(char character, unsigned int times);
+#define uint unsigned int
+
+uint maxHeight;
+
+static uint GetHeight();
+static void PrintChar(char characterToPrint, uint timesToPrint);
+static void PrintPyramid(uint maxHeight);
 
 int main(void)
 {
-	unsigned int height;
-	int result;
-	char line[3];
+	maxHeight = GetHeight();
+
+	PrintPyramid(maxHeight);
+}
+
+static uint GetHeight()
+{
+	int sscanResult;
+	char inputLine[3];
 
 	do
 	{
 		printf_s("Enter a number between 1 and 8.\n");
 		printf_s("Height: ");
-		fgets(line, 3, stdin);
+		fgets(inputLine, 3, stdin);
 		/* 1st char is our height, 2nd is a new line (\n),
 		3rd is the null char (\0), which is added automatically
 		by fgets() as strings in C should be null-terminated.
 		Which means if we read only 2 chars, the last char will be \0
 		and any input will be discarded by the next if statement */
 
-		result = sscanf_s(line, "%d", &height);
+		sscanResult = sscanf_s(inputLine, "%d", &maxHeight);
 
-		int len = strlen(line);
-		if (line[len - 1] == '\n')
+		int inputLength = strlen(inputLine);
+		if (inputLine[inputLength - 1] == '\n')
 		{
-			line[len - 1] = 0;
+			inputLine[inputLength - 1] = 0;
 		}
 		else
 		{
 			scanf_s("%*[^\n]");
 			scanf_s("%*c"); //clear up to newline
 		}
-	} while (result != 1 || height < 1 || height > 8);
+	}
+	while (sscanResult != 1 || maxHeight < 1 || maxHeight > 8);
 
-	for (unsigned int currentHeight = 0; currentHeight < height; currentHeight++)
+	return maxHeight;
+}
+
+static void PrintChar(char characterToPrint, uint timesToPrint)
+{
+	if (timesToPrint == 0)
 	{
-		PrintChar(' ', height - currentHeight - 1);
-		/* print (height - currentHeight) spaces,
+		return;
+	}
+	for (uint i = 0; i < timesToPrint; i++)
+	{
+		printf("%c", characterToPrint);
+	}
+}
+
+static void PrintPyramid(uint maxHeight)
+{
+	for (uint currentHeight = 0; currentHeight < maxHeight; currentHeight++)
+	{
+		PrintChar(' ', maxHeight - currentHeight - 1);
+		/* print (maxHeight - currentHeight - 1) spaces,
 		e.g. if height == 4 and it's a first iteration,
 		we need to print 3 spaces; this is made to align our pyramid */
 		PrintChar('#', currentHeight + 1);
 		printf("  ");
 		PrintChar('#', currentHeight + 1);
 		printf("\n");
-	}
-}
-
-static void PrintChar(char character, unsigned int times)
-{
-	if (times == 0)
-	{
-		return;
-	}
-	for (unsigned int i = 0; i < times; i++)
-	{
-		printf("%c", character);
 	}
 }
