3f6f8f0c051c5aec0ba55ce096ca1c736074b308
gen-kai <87318319+gen-kai@users.noreply.github.com>
2025-06-07 13:19:38 -0700

    Fix mistakingly changed function name

--- Lecture 2/readability/readability.c
+++ Lecture 2/readability/readability.c
@@ -8,7 +8,7 @@ typedef unsigned int uint;
 
 #define TEXT_MAX_SIZE 2001
 
-uint PromptForKey(char *enteredText);
+uint PromptForText(char *enteredText);
 
 bool IsEndOfWord(char stringChar);
 bool IsEndOfSentence(char stringChar);
@@ -28,7 +28,7 @@ int main(void)
 	float lettersPerHundredWords, sentencesPerHundredWords;
 	float textComplexity;
 
-	enteredTextLength = PromptForKey(enteredText);
+	enteredTextLength = PromptForText(enteredText);
 
 	letterCount = CountLetters(enteredText, enteredTextLength);
 	wordCount = CountWords(enteredText, enteredTextLength);
@@ -53,7 +53,7 @@ int main(void)
 	}
 };
 
-uint PromptForKey(char *enteredText)
+uint PromptForText(char *enteredText)
 {
 	char inputLine[TEXT_MAX_SIZE];
 
